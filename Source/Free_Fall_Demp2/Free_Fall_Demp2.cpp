b0b214a23b9aa4e74739c09ae63ae56243d38803
LMTrefry <Luke.M.Trefry@liosn.enc.edu>
2020-10-21 14:27:46 -0400

    Changed to Blueprint

--- Source/Free_Fall_Demp2/Free_Fall_Demp2.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Free_Fall_Demp2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Free_Fall_Demp2, "Free_Fall_Demp2" );
- 
